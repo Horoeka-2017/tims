f10e4c59d8c17d7e2838c668fc53bc5c885169c1
Joshua-Fogg <joshuafoggnz@gmail.com>
2017-08-15 11:19:19 +1200

    add mock people data && route people data

--- server/apiRoutes.js
+++ server/apiRoutes.js
@@ -1,15 +1,17 @@
 const express = require('express')
-const request = require('superagent')
 const bodyParser = require('body-parser')
 
 const router = express.Router()
 
+const db = require('./db')
 const mockMessageData = [{
-  sender: {id: 1, name: 'some', photo: 'stuff'},
-  recipient: {id: 2, name: 'in', photo: 'here'},
+  sender: { id: 1, name: 'some', photo: 'stuff' },
+  recipient: { id: 2, name: 'in', photo: 'here' },
   imageUrl: 'http://via.placeholder.com/300x300',
   dateTime: Date.now()
-}]
+  }]
+
+const mockPeopleData = [{ id: 1, name: 'Seth', photo: 'NitinAndNathan.jpg' }, { id: 3, name: 'Paul', photo: 'BrabetBeard.jpg' }]
 
 router.use(bodyParser.json())
 
@@ -18,11 +20,23 @@ router.get('/messages', (req, res) => {
 })
 
 router.post('/messages', (req, res) => {
-
+  const message = {
+    senderId: req.body.senderId,
+    recipientId: req.body.recipientId,
+    imageUrl: req.body.imageUrl
+  }
+  db.createMessage(message)
+    .end((err, res) => {
+      if (err) {
+        res.status(500).err.message
+      } else {
+        res.status(201).send('Message Added')
+      }
+    })
 })
 
 router.get('/people', (req, res) => {
-
+  res.json(mockPeopleData)
 })
 
 module.exports = router
