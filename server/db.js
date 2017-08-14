d685a9dccadd06c25c9c0aec47a0adcf814ce833
Joshua-Fogg <joshuafoggnz@gmail.com>
2017-08-15 11:44:42 +1200

    add knex to dependency list

--- server/db.js
+++ server/db.js
@@ -1,3 +1,18 @@
-module.exports = {
+var environment = process.env.NODE_ENV || 'development'
+var config = require('../../knexfile')[environment]
+var connection = require('knex')(config)
 
+module.exports = {
+  getMessages, createmessage, getPeople
 }
+
+function getMessages (connection) {
+  return 
+}
+
+function createmessage (message, conn) { 
+const db = conn || connection
+db('message')
+}
+
+function getPeople () { }
