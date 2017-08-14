5bc2a64c20d17bcc26e451b64cd06ac6fd2c9432
Nathan Lamb <nathanlamb022@gmail.com>
2017-08-15 11:08:14 +1200

    Remove initial period from photo path

--- client/people.js
+++ client/people.js
@@ -2,61 +2,61 @@ people = [
   {
     id: 1,
     name: 'brenna',
-    photo: './images/brenna.jpg'
+    photo: '/images/brenna.jpg'
   },
   {
     id: 2,
     name: 'carmen',
-    photo: './images/carmen.jpg'
+    photo: '/images/carmen.jpg'
   },
   {
     id: 3,
     name: 'carter',
-    photo: './images/carter.jpg'
+    photo: '/images/carter.jpg'
   },
   {
     id: 4,
     name: 'doreen',
-    photo: './images/doreen.jpg'
+    photo: '/images/doreen.jpg'
   },
   {
     id: 5,
     name: 'frederick',
-    photo: './images/frederick.jpg'
+    photo: '/images/frederick.jpg'
   },
   {
     id: 6,
     name: 'jared',
-    photo: './images/.jpg'
+    photo: '/images/jared.jpg'
   },
   {
     id: 7,
     name: 'jasper',
-    photo: './images/jasper.jpg'
+    photo: '/images/jasper.jpg'
   },
   {
     id: 8,
     name: 'judy',
-    photo: './images/judy.jpg'
+    photo: '/images/judy.jpg'
   },
   {
     id: 9,
     name: 'kendra',
-    photo: './images/kendra.jpg'
+    photo: '/images/kendra.jpg'
   },
   {
     id: 10,
     name: 'leo',
-    photo: './images/leo.jpg'
+    photo: '/images/leo.jpg'
   },
   {
     id: 11,
     name: 'rachel',
-    photo: './images/rachel.jpg'
+    photo: '/images/rachel.jpg'
   },
   {
     id: 12,
     name: 'shelton',
-    photo: './images/shelton.jpg'
+    photo: '/images/shelton.jpg'
   }
 ]
