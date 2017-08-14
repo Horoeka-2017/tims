c5a5aefb2b34b9302bed673ed44d64c365e2cd8a
Joshua-Fogg <joshuafoggnz@gmail.com>
2017-08-15 10:38:47 +1200

    add mock messageData

--- server/apiRoutes.js
+++ server/apiRoutes.js
@@ -4,10 +4,17 @@ const bodyParser = require('body-parser')
 
 const router = express.Router()
 
+const mockMessageData = [{
+  sender: {id: 1, name: 'some', photo: 'stuff'},
+  recipient: {id: 2, name: 'in', photo: 'here'},
+  imageUrl: 'http://via.placeholder.com/300x300',
+  dateTime: Date.now()
+}]
+
 router.use(bodyParser.json())
 
 router.get('/messages', (req, res) => {
-  res.json(mockData)
+  res.json(mockMessageData)
 })
 
 router.post('/messages', (req, res) => {
