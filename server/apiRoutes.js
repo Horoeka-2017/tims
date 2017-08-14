23f22dad12785292f61b1dfa94eab7e37deae116
Joshua-Fogg <joshuafoggnz@gmail.com>
2017-08-15 10:22:35 +1200

    add post message route

--- server/apiRoutes.js
+++ server/apiRoutes.js
@@ -7,15 +7,18 @@ const router = express.Router()
 router.use(bodyParser.json())
 
 router.get('/messages', (req, res) => {
-  request
-    .get(`http://www.reddit.com/r/${req.params.subreddit}.json`)
-    .end((err, result) => {
-      if (err) {
-        res.status(500).send(err.message)
-      } else {
-        res.json(result.body.data.children)
-      }
-    })
+  // request
+  //   .get(`http://www.reddit.com/r/${req.params.subreddit}.json`)
+  //   .end((err, result) => {
+  //     if (err) {
+  //       res.status(500).send(err.message)
+  //     } else {
+  //       res.json(result.body.data.children)
+  //     }
+  //   })
+})
+router.post('/messages', (req, res) => {
+
 })
 
 module.exports = router
