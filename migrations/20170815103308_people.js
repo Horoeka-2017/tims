aaa6c62746ea43a78e04b457b939ab7e54f25610
james-inglis-01 <james.a.i.inglis@gmail.com>
2017-08-15 11:20:56 +1200

    fixing linting/indenting

--- migrations/20170815103308_people.js
+++ migrations/20170815103308_people.js
@@ -1,11 +1,11 @@
 exports.up = function(knex, Promise) {
-    return knex.schema.createTableIfNotExists('People', function(table) {
-        table.increments('id').primary()
-        table.string('name')
-        table.string('photo')
-    })
+  return knex.schema.createTableIfNotExists('People', function(table) {
+    table.increments('id').primary()
+    table.string('name')
+    table.string('photo')
+  })
 }
 
 exports.down = function(knex, Promise) {
-    return knex.schema.dropTableIfExists('people')
+  return knex.schema.dropTableIfExists('people')
 }
--- migrations/20170815103513_messages.js
+++ migrations/20170815103513_messages.js
@@ -1,13 +1,13 @@
 exports.up = function(knex, Promise) {
-    return knex.schema.createTableIfNotExists('messages', function(table) {
-        table.increments('id').primary()
-        table.integer('sender_id')
-        table.integer('recipient_id')
-        table.string('image_url')
-        table.string('date_time')
-    })
+  return knex.schema.createTableIfNotExists('messages', function(table) {
+    table.increments('id').primary()
+    table.integer('sender_id')
+    table.integer('recipient_id')
+    table.string('image_url')
+    table.string('date_time')
+  })
 }
 
 exports.down = function(knex, Promise) {
-    return knex.schema.dropTableIfExists('messages')
+  return knex.schema.dropTableIfExists('messages')
 }
