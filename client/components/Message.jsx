9bc71e7195925f084070e52cb3bc793359e8645d
Nathan Lamb <nathanlamb022@gmail.com>
2017-08-16 11:13:11 +1200

    Change function to class

--- client/components/Message.jsx
+++ client/components/Message.jsx
@@ -2,16 +2,25 @@ import React from 'react'
 import Person from './Person'
 import PropTypes from 'prop-types'
 
-export default function Message (props) {
-  return (
+class Message extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+    }
+  }
+  render () {
+    return (
     <div>
       <Person person={props.message.sender}/>
       <img src={props.message.imageUrl}/>
       <Person person={props.message.recipient}/>
+      <button value='delete' onClick={this.handleOnClick}>x</button>
     </div>
-  )
+    )
+  }
 }
-
 Message.propTypes = {
   message: PropTypes.object.isRequired
 }
+
+export default Message
