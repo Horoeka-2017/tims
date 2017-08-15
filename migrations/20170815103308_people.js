9d8e01e92297b4361b834f564bb07cd60ae8eabd
james-inglis-01 <james.a.i.inglis@gmail.com>
2017-08-15 14:48:38 +1200

    Made changes based on Don's recommedations

--- migrations/20170815103308_people.js
+++ migrations/20170815103308_people.js
@@ -1,5 +1,5 @@
 exports.up = function(knex, Promise) {
-  return knex.schema.createTableIfNotExists('People', function(table) {
+  return knex.schema.createTableIfNotExists('people', function(table) {
     table.increments('id').primary()
     table.string('name')
     table.string('photo')
