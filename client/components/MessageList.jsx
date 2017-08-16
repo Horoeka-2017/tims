f21ade2c636f74aa40ef189d7ff04245d90c85c5
Max Tweddell <maxt.nz@gmail.com>
2017-08-16 15:22:58 +1200

    removed accidently deleting the whole messagelist component

--- client/components/MessageList.jsx
+++ client/components/MessageList.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {Link} from 'react-router-dom'
 
-import LoadMessages ed
+import LoadMessages from './LoadMessages'
+import Message from './Message'
+import { connect } from 'react-redux'
+
+function MessageList ({ messages }) {
+  return (
+    <div>
+      <div>
+        {messages.map((message, i) => {
+          return <Message key={i} message={message} />
+        })
+        }
+      </div>
+      <LoadMessages/>
+      <Link to='/new'> Create New Message </Link>
+    </div>
+  )
+}
+
+MessageList.propTypes = {
+  messages: PropTypes.array.isRequired
 }
 
 function mapStateToProps (state) {
