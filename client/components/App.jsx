442fd07a7343875fd29804ef1947766cc40c8fa7
Max Tweddell <maxt.nz@gmail.com>
2017-08-16 15:10:42 +1200

    removed unused links from new page

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 import MessageList from './MessageList'
 import {Route, Link} from 'react-router-dom'
 import NewMessage from './NewMessage'
-import LoadMessages from './LoadMessages'
 
 export default class App extends React.Component {
   render () {
@@ -11,8 +10,6 @@ export default class App extends React.Component {
       <div className='app'>
         <Route exact path ='/' component={MessageList}/>
         <Route path='/new' component={NewMessage} />
-        <Link to='/new'> Create New Message </Link>
-        <LoadMessages/>
       </div>
     )
   }
--- client/components/MessageList.jsx
+++ client/components/MessageList.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import {Link} from 'react-router-dom'
 
+import LoadMesssages from './LoadMessages'
 import Message from './Message'
 import { connect } from 'react-redux'
 
 function MessageList ({ messages }) {
   return (
     <div>
-      {messages.map((message, i) => {
-        return <Message key={i} message={message} />
-      })
-      }
+      <div>
+        {messages.map((message, i) => {
+          return <Message key={i} message={message} />
+          })
+        }
+      </div>
+      <Link to='/new'> Create New Message </Link>
     </div>
   )
 }
