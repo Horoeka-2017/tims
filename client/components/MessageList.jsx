7729725660db859e50fe3d06086cd87242ca3208
Max Tweddell <maxt.nz@gmail.com>
2017-08-16 15:21:59 +1200

    removed sneaky 's'

--- client/components/MessageList.jsx
+++ client/components/MessageList.jsx
@@ -2,27 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {Link} from 'react-router-dom'
 
-import LoadMesssages from './LoadMessages'
-import Message from './Message'
-import { connect } from 'react-redux'
-
-function MessageList ({ messages }) {
-  return (
-    <div>
-      <div>
-        {messages.map((message, i) => {
-          return <Message key={i} message={message} />
-        })
-        }
-      </div>
-      <LoadMessages/>
-      <Link to='/new'> Create New Message </Link>
-    </div>
-  )
-}
-
-MessageList.propTypes = {
-  messages: PropTypes.array.isRequired
+import LoadMessages ed
 }
 
 function mapStateToProps (state) {
