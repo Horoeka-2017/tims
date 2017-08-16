e280b325866f8cd5485efde9377ad482a58f0a58
Joshua-Fogg <joshuafoggnz@gmail.com>
2017-08-16 12:28:52 +1200

    pull dev save

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -11,18 +11,10 @@ export default class App extends React.Component {
   render () {
     return (
       <div className='app'>
-<<<<<<< HEAD
-        <ErrorMessage />
-        <LoadMessages/>
-        <WaitIndicator />
-        <MessageList/>
-        <NewMessage />
-=======
         <Route exact path ='/' component={MessageList}/>
         <Route path='/new' component={NewMessage} />
         <Link to='/new'> Create New Message </Link>
         <LoadMessages/>
->>>>>>> faaab6205aeda976ac12a8bec2c8bb833973bab7
       </div>
     )
   }
