04acb5a911a89eac6d9d2304ed5459fca88d6ee2
Joshua-Fogg <joshuafoggnz@gmail.com>
2017-08-16 15:32:21 +1200

    add actions for people calls

--- client/actions/index.js
+++ client/actions/index.js
@@ -3,29 +3,43 @@ import request from 'superagent'
 export const GET_MESSAGES = 'GET_MESSAGES'
 export const RECEIVE_MESSAGES = 'RECEIVE_POSTS'
 export const REQUEST_MESSAGES = 'REQUEST_MESSAGES'
+export const RECEIVE_PEOPLE = 'RECEIVE_PEOPLE'
+export const REQUEST_PEOPLE = 'REQUEST_PEOPLE'
 export const SHOW_ERROR = 'SHOW_ERROR'
 
-export function requestMessages () {
+export function requestMessages() {
   return {
     type: REQUEST_MESSAGES
   }
 }
 
-export function receiveMessages (messages) {
+export function receiveMessages(messages) {
   return {
     type: RECEIVE_MESSAGES,
     messages: messages
   }
 }
 
-export function showError (errorMessage) {
+export function showError(errorMessage) {
   return {
     type: SHOW_ERROR,
     errorMessage: errorMessage
   }
 }
 
-export function fetchMessages () {
+export function requestPeople() {
+  return {
+    type: REQUEST_PEOPLE
+  }
+}
+export function receivePeople(people) {
+  return {
+    type: RECEIVE_PEOPLE,
+    people: people
+  }
+}
+
+export function fetchMessages() {
   return (dispatch) => {
     dispatch(requestMessages())
     request
@@ -35,6 +49,20 @@ export function fetchMessages () {
           dispatch(showError(err.message))
         } else {
           dispatch(receiveMessages(res.body))
+        }
+      })
+  }
+}
+
+export function getPeople() {
+  return (dispatch) => {
+    dispatch(requestPeople())
+    request.get('/api/v1/people')
+      .end((err, res) => {
+        if (err) {
+          dispatch(showError(err.message))
+        } else {
+          dispatch(receivePeople(res.body))
         }
       })
   }
