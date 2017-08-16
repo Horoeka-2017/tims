bc556c744b13bd4693acb44e76ed0dede9316dfe
emma-single <emmamollysingle@gmail.com>
2017-08-16 12:00:34 +1200

    took away the "hi" test

--- client/components/MessageList.jsx
+++ client/components/MessageList.jsx
@@ -2,15 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import Message from './Message'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 
-function MessageList ({messages}) {
+function MessageList ({ messages }) {
   return (
     <div>
-      hi
       {messages.map((message, i) => {
-        return <Message key = {i} message={message}/>
-        })
+        return <Message key={i} message={message} />
+      })
       }
     </div>
   )
