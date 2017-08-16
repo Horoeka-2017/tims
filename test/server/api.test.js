ad5a1ba0d4129c61eab9f523f6e801dfc0ab8ba8
Nathan Lamb <nathanlamb022@gmail.com>
2017-08-16 13:39:51 +1200

    Add tests

--- test/server/api.test.js
+++ test/server/api.test.js
@@ -3,9 +3,62 @@ var test = require('tape')
 
 var app = require('../../server/server.js')
 
+test('/messages returns status 200', function (t) {
+  const expected = 200
+
+  request(app)
+    .get('/api/v1/messages')
+    .end(function (err, res) {
+      t.error(err)
+      t.equal(res.status, expected)
+      t.end()
+    })
+})
+
+test('/messages returns an object', function (t) {
+  const expected = 'object'
+
+  request(app)
+  .get('/api/v1/messages')
+  .end(function (err, res) {
+    t.error(err)
+    t.equal(typeof (res.body), expected)
+    t.end()
+  })
+})
+
+test('/messages post returns status 200', function (t) {
+  const expected = 201
+  const message = {
+    senderId: 10,
+    recipientId: 10,
+    imageUrl: 'string'
+  }
+  request(app)
+    .post('/api/v1/messages')
+    .send(message)
+    .end(function (err, res) {
+      t.error(err)
+      t.equal(res.status, expected)
+      t.end()
+    })
+
+  test('/people returns an object', function (t) {
+    const expected = 'object'
+
+    request(app)
+        .get('/api/v1/people')
+        .end(function (err, res) {
+          t.error(err)
+          t.equal(typeof (res.body), expected)
+          t.end()
+        })
+  })
+})
+
 test('/messages/1 returns 200', function (t) {
   // Arrange
-  var expected = 200
+  const expected = 200
 
   // Act
   request(app)
