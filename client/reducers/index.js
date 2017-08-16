0b7d862609ee35b3cf71d6b3fbb03e84b1ef6507
Joshua-Fogg <joshuafoggnz@gmail.com>
2017-08-16 15:38:45 +1200

    add reducers and actions

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -3,9 +3,11 @@ import {combineReducers} from 'redux'
 import errorMessage from './error-message'
 import waiting from './waiting'
 import messages from './messages'
+import people from './people'
 
 export default combineReducers({
   errorMessage,
   waiting,
-  messages
+  messages,
+  people
 })
