1c022e209abde15c871389d362bb11ea5f32fbda
Joshua-Fogg <joshuafoggnz@gmail.com>
2017-08-16 16:40:33 +1200

    add title

--- client/components/NewMessage.jsx
+++ client/components/NewMessage.jsx
@@ -60,6 +60,11 @@ class NewMessage extends React.Component {
   render () {
     return (
       <div>
+        <div className='title'><h1>Welcome To TIMS New Message Form</h1>
+        <h2>Add a new message with the form below</h2>
+        <br />
+        <p>identify yourself as the sender, select a recipient to send a message too, upload an image and then click submit :)</p>
+        </div>
         <div>
           <select name="sender" onChange={this.handleChange}>
             <option value="sender">Sender</option>
